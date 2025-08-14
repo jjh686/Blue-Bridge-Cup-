246ae2f383479230557764c3b4f84076a67b3031
jjh686 <2769430595@qq.com>
2025-08-14 22:34:43 +0800

    Add files via upload

---
+++ text/main.c
@@ -0,0 +1,6 @@
+#include "STC15F2K60S2.H"
+
+void main()
+{
+	
+}
---
+++ 独立按键/三行按键法/Library/System/Delay.h
@@ -0,0 +1,12 @@
+#ifndef __DEALY_H_
+#define __DEALY_H_
+
+#include <intrins.h>
+
+void vDelay_Ms(unsigned int ms);		
+void vDelay_Us(unsigned int us);	
+void vDelay_10us(unsigned int us);	
+void vDelay_100us(unsigned int us);	
+
+
+#endif
